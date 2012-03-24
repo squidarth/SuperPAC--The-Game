d963daf22c9e1dddec7ff011ce75e1dca3a11d27
Sidharth Shanker <sshanker220@gmail.com>
2012-03-24 16:22:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superpac::Application.config.secret_token = '424a8686c96fedae328eee6db0539e3361cf3ff96f0fde8a4cfe36a0fe4dabb81b01b399e7baba5bdf323e482c93b9ddbcae1acae079e36de2db50c6f22dbf1c'
